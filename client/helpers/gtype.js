f471d02f9e402b22f8763913a791573e86022055
sulerzh <sulerzh@sina.com>
2015-07-23 16:17:18 +0800

    梳理目录结构

---
+++ client/helpers/gtype.js
@@ -0,0 +1,6 @@
+﻿GType = {
+  placeType: ["Private Residency", "Cafe", "Restaurant", "Other", "Studio", "Outdoor"],
+  hostingStyle: ["Plated", "Self-served"],
+  gender: ["Male", "Female", "Other", "Prefer Not to Tell"],
+  notificationType: ["Review", "Request", "Reminder", "Comment"]
+};
---
+++ client/helpers/logger.js
@@ -0,0 +1,10 @@
+﻿function OutputLogger(debugLog) {
+  this.debug = debugLog;
+  this.log = function (info) {
+    if (this.debug) {
+      console.log(info);
+    }
+  }
+};
+
+Logger = new OutputLogger(Meteor.settings.deubgLog);
